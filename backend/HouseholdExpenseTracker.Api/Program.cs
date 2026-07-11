eedded6613aa209dd84232b16f2224b17e362711
beatriz.lopes30 <trabalhosdabia@gmail.com>
2026-07-11 19:22:29 -0300

    feat: configure Program.cs with database, services, CORS and JSON options

--- backend/HouseholdExpenseTracker.Api/Program.cs
+++ backend/HouseholdExpenseTracker.Api/Program.cs
@@ -1,23 +1,57 @@
+using HouseholdExpenseTracker.Api.Data;
+using HouseholdExpenseTracker.Api.Middleware;
+using HouseholdExpenseTracker.Api.Services;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+const string FrontendCorsPolicy = "FrontendCorsPolicy";
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Database
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Services
+builder.Services.AddScoped<PersonService>();
+builder.Services.AddScoped<TransactionService>();
+builder.Services.AddScoped<TotalsService>();
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(FrontendCorsPolicy, policy =>
+        policy.WithOrigins("http://localhost:5173", "http://127.0.0.1:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+
+// Controllers
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
-
+app.UseCors(FrontendCorsPolicy);
+app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseAuthorization();
-
 app.MapControllers();
+
+// Migrations 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
 
 app.Run();
